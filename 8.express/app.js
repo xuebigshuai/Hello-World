9a275a9d4612a5157c7257fec996ebef2ac1f03d
罗佳 <18700870001@163.com>
2018-09-13 19:44:27 +0800

    express框架 模板的使用

--- 8.express/app.js
+++ 8.express/app.js
@@ -1,7 +1,12 @@
 var express = require("express");
-
 var bodyParser = require("body-parser");
 var app = express();
-
-app.use(bodyParser);
-app.get();
+var path = require("path");
+var data = {"title":"模板","name":"薛煜帅","age":24,"pin":"xueyushuai"};
+app.set('view engine','ejs');
+app.set('views',__dirname);
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/index",function (req, res) {
+    res.render('index',data);
+});
+app.listen(8282);
