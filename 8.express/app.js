1d1c127bca3c49ba40a3bffac5ecfa5abbdb7fc0
罗佳 <18700870001@163.com>
2018-09-17 19:53:57 +0800

    express框架 restful风格

--- 8.express/app.js
+++ 8.express/app.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
+app.listen(8282);
 var path = require("path");
+var fs= require("fs");
 var data = {"title":"模板","name":"薛煜帅","age":24,"pin":"xueyushuai"};
 app.set('view engine','ejs');
 app.set('views',__dirname);
@@ -9,4 +11,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.get("/index",function (req, res) {
     res.render('index',data);
 });
-app.listen(8282);
+app.get("/user",function (req,res) {
+    fs.createReadStream("./restful/user.json").pipe(res);
+});
+app.get("/user/:id",function (req,res) {
+   var users = require("./restful/user");
+   user = users.filter(function (item) {
+       return item.id == req.params.id
+   });
+   res.send(user);
+});
---
+++ 8.express/restful/restful.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app =express();
+/*解析post请求体中的参数*/
+var bodyParser=require('body-parser');
+var db = "./user.json";
+let fs = require('fs');
+let path =require('path');
+app.use(bodyParser.urlencoded({extended:true}));
+app.post("/user",function (req,res) {
+   var newUser = req.body;
+   var users = require(db);
+   newUser.id = users[users.length-1].id*1 + 1;
+   users.push(newUser);
+   console.log(users);
+   var str = JSON.stringify(users);
+   fs.writeFile(db,str,function (error) {
+       res.send(newUser);
+   });
+});
+app.listen(8080);
