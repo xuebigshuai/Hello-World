83937efc6373c7c893e7f9d680e68ba6874e7710
罗佳 <18700870001@163.com>
2018-10-28 11:02:07 +0800

    js之变量提升

--- ES6/变量提升/变量提升.js
+++ ES6/变量提升/变量提升.js
@@ -8,4 +8,20 @@ var a = new Object() ;
 function a () {
     console.log(arguments);
 };
-//console.log(a,a.o);
+console.log(a,a.o);
+
+console.log(bb);
+bb();
+var bb = 1;
+console.log(bb);
+function bb() {
+    console.log(2);
+}
+console.log(bb);
+bb = 3 ;
+console.log(bb);
+function bb() {
+    console.log(4);
+}
+console.log(bb);
+
