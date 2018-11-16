95e5a5d5ffb349b3c2fbff1783811d91a217f052
罗佳 <18700870001@163.com>
2018-11-16 17:48:19 +0800

    当前目录所在的绝对路径

--- 1.global/global.js
+++ 1.global/global.js
@@ -6,3 +6,7 @@ console.info("info");
 console.error('error');
 console.warn("warn");
 require("util");
+//当前目录所在的绝对路径
+console.log(__dirname);//E:\learn\gitproject\demo1\20180523git\Hello-World\1.global
+//当前文件所在的绝对路径
+console.log(__filename);//E:\learn\gitproject\demo1\20180523git\Hello-World\1.global\global.js
--- 8.express/app.js
+++ 8.express/app.js
@@ -11,9 +11,11 @@ var path = require("path");
 var fs= require("fs");
 var data = {"title":"模板","name":"薛煜帅","age":24,"pin":"xueyushuai"};
 app.set('view engine','ejs');
+console.log(__dirname);
 app.set('views',__dirname);
 app.use(bodyParser.urlencoded({extended:true}));
 app.get("/index",function (req, res) {
+    //第二参数为传入模板的对象
     res.render('index',data);
 });
 app.get("/user",function (req,res) {
